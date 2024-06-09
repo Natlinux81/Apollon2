2b0d65ef9b61a647c207068a20a950ad78fc0cc9
natlinux <nathaliewenske790@hotmail.com>
2024-06-09 21:49:36 +0200

    home-component and footer style

--- Apollon2UI/src/app/app.component.ts
+++ Apollon2UI/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent],
+  imports: [RouterOutlet,NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
